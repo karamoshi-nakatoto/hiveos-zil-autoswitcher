c6824dbb136a9f6ffa5a19a80467c1741442129a
Karamoshi Nakatoto <karamoshi.nakatoto@protonmail.com>
2023-04-16 21:14:44 +0300

    feat: revert estimation logic back to crazypool

--- daemon.sh
+++ daemon.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 # shellcheck shell=sh
+# TODO: add strict mode
 
 apiUrl="$HIVEOS_API_URL"
 authToken="$HIVEOS_AUTH_TOKEN"
@@ -15,24 +16,11 @@ switchFs() {
 }
 
 getTimeUntilNextEpoch() {
-  lastEpochBlock=$(curl 'https://api.zilliqa.com/' -sSL -X POST \
-    -H 'Content-Type: application/json' \
-    --data-raw '{"id":1,"jsonrpc":"2.0","method":"GetBlockchainInfo","params":[]}' | grep -Eo '"CurrentDSEpoch":"[0-9]+"' | cut -b 18-)
-
-  lastEpoch=$(curl 'https://api.zilliqa.com/' -sSL -X POST \
-    -H 'Content-Type: application/json' \
-    --data-raw '{"id":1,"jsonrpc":"2.0","method":"GetDsBlock","params":['"$lastEpochBlock]}" | grep -Eo '"Timestamp":"[0-9]+' | cut -b 14-)
-
-  unix=$((lastEpoch / 1000000))
-
+  nextEpoch="$(curl -sS 'https://zil.crazypool.org/api/stats/chart' | grep -Eo "\"nextEpochTime\":[0-9]+" | cut -b 17-)"
   now="$(date +%s)"
-  diff=$((now - unix))
-  # We estimate that zil epochs are about 3960 seconds and a mining window is
-  # about 120 seconds.
-  # Source: https://devex.zilliqa.com/dsbk?network=https%3A%2F%2Fapi.zilliqa.com
-  untilNext=$((unix - now + 3960 - 120))
+  diff=$((nextEpoch-now))
 
-  echo "$untilNext"
+  echo $diff
 }
 
 while true;
@@ -46,18 +34,8 @@ do
     if [ "$diff" -lt 100 ]; then
       echo switching to nexa+zil
       switchFs "$primaryAndZilFs"
-      echo waiting for next block...
-      while true;
-      do
-        diff=$(getTimeUntilNextEpoch)
-        if [ "$diff" -gt 1000 ]; then
-          echo new block found
-          break
-        fi
-        
-        echo .
-        sleep 5
-      done
+
+      sleep $((diff+120))
 
       echo switching back to nexa only
       switchFs "$primaryFs"
